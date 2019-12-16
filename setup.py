482dfd18f0c1383c84627dcac2baceb0b9881986
Malte Franken <exxamalte@users.noreply.github.com>
2019-12-16 19:33:24 +1100

    initial configuration

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+
+from setuptools import setup, find_packages
+
+NAME = "aio_georss_gdacs"
+AUTHOR = "Malte Franken"
+AUTHOR_EMAIL = "coding@subspace.de"
+DESCRIPTION = "An async GeoRSS client library for GDACS feeds."
+URL = "https://github.com/exxamalte/python-aio-georss-gdacs"
+
+REQUIRES = [
+    'aio_georss_client>=0.3',
+    'dateparser>=0.7.0',
+]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+VERSION = {}
+with open(os.path.join(HERE, NAME, "__version__.py")) as f:
+    exec(f.read(), VERSION)  # pylint: disable=exec-used
+
+setup(
+    name=NAME,
+    version=VERSION["__version__"],
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIRES
+)
