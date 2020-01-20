69addd9c8e93f5964ff311e281912e2714bdb274
Malte Franken <exxamalte@users.noreply.github.com>
2020-01-20 18:56:40 +1100

    bump version of upstream library

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = "An async GeoRSS client library for GDACS feeds."
 URL = "https://github.com/exxamalte/python-aio-georss-gdacs"
 
 REQUIRES = [
-    'aio_georss_client>=0.4',
+    'aio_georss_client>=0.5',
     'dateparser>=0.7.0',
 ]
 
